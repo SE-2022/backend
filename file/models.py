3474c939086b7fdc7767f2b6af40f2c81d88c284
VeraMayLin <1599763731@qq.com>
2022-05-09 20:02:14 +0800

    file and directory database modify

--- file/models.py
+++ file/models.py
@@ -15,6 +15,13 @@ class File(models.Model):
     TeamID = models.ForeignKey(Team, to_field='teamID', on_delete=models.CASCADE)
     commentFul = models.BooleanField(default=True)
     content = models.TextField()
+    isDelete = models.BooleanField(default=False)  # If the file has been deleted, this value is True.
+
+    class Meta:
+        db_table = 'File'
+
+    def __unicode__(self):
+        return 'file_name:%s' % self.file_name
 
 
 class Comment(models.Model):
@@ -22,3 +29,17 @@ class Comment(models.Model):
     comment_time = models.DateTimeField(auto_now_add=True)
     comment_fileID = models.ForeignKey(File, to_field='FileID', on_delete=models.CASCADE)
     comment_user = models.ForeignKey(User, to_field='userID', on_delete=models.CASCADE)
+
+
+class Directory(models.Model):
+    dir_name = models.CharField(max_length=100)
+    dirID = models.AutoField(primary_key=True, editable=False, null=False)
+    create_time = models.DateTimeField(auto_now_add=True)
+    file_num = models.IntegerField(default=0)
+    userID = models.ForeignKey(User, on_delete=models.CASCADE, to_field='userID')
+
+    class Meta:
+        db_table = 'Directory'
+
+    def __unicode__(self):
+        return 'dir_name:%s|file_num:%s' % (self.dir_name, self.file_num)
