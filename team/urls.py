07b881b804c7421508322100cc50ff514022fb01
Li Yu Heng <liyuheng55555@126.com>
2022-06-07 21:47:14 +0800

    增加移交管理权（manager_transfer）和销毁团队（destroy）两个方法

--- team/urls.py
+++ team/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import *
-
 urlpatterns = [
     # 指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
     path('create_team', create_team),
@@ -10,6 +9,9 @@ urlpatterns = [
     path('team_info', team_info),
     path('invite', invite),
     path('is_manager', is_manager),
+    path('manager_transfer', manager_transfer),
+    path('destroy', destroy),
     path('debug_all_team', debug_all_team),
     path('debug_clear_team', debug_clear_team),
 ]
+
